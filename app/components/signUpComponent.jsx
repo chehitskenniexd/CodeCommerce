ce44feb028fad7b3ca1cf07b0ac5573db64e28d4
Kenneth Moy <moykenneth91@gmail.com>
2016-11-07 21:55:31 -0500

    fixed multiple errors regarding buttons

--- app/components/signUpComponent.jsx
+++ app/components/signUpComponent.jsx
@@ -43,28 +43,11 @@ export default class SignUpComponent extends React.Component {
               onChange={(event) => { this.onHandleChange('password', event) } } />
 
             <input className="btn btn-lg btn-primary btn-block" type="submit" value="Sign Up" />
+            <input className="btn btn-lg btn-primary btn-block" type="button" value="Sign Up with Facebook" />
+            <input className="btn btn-lg btn-primary btn-block" type="button" value="Sign Up with Google+" />
           </form>
-          
         </div>
       </div>
     )
   }
 }
-
-/*
-             <h3 className="form-signin-heading">Welcome to Swag-Store! Please Sign Up</h3>
-              <hr className="colorgraph"></hr>
-              
-              <input className="form-control" name="firstName" placeholder="First Name" 
-                onChange={(event) => {this.onHandleChange('firstName', event)}}/>
-              <input className="form-control" name="lastName" placeholder="Last Name" 
-                onChange={(event) => {this.onHandleChange('lastName', event)}}/>
-              <input className="form-control" name="email" placeholder="Email Address" 
-                onChange={(event) => {this.onHandleChange('email', event)}}/>
-              <input className="form-control" name="password"  type="password" placeholder="Password" 
-                onChange={(event) => {this.onHandleChange('password', event)}}/>
-
-              <input className="btn btn-lg btn-primary btn-block" type="submit" value="Sign Up"/>
-              <input className="btn btn-lg btn-primary btn-block" value="Sign Up with Facebook"/>
-              <input className="btn btn-lg btn-primary btn-block" value="Sign Up with Google+"/>
-*/
