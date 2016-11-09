1a99655ff11f5f7aad38444a82bd87f3aaa5aa74
George Smith-Sweeper <george.smsweeper@gmail.com>
2016-11-08 19:57:34 -0500

    orderComponent styles

--- app/components/orderComponent.jsx
+++ app/components/orderComponent.jsx
@@ -12,7 +12,7 @@ export default class OrderComponent extends React.Component {
             total: 0
         }
     }
-    
+
     componentDidMount() {
         // Temporary axios call
         axios.get(`/api/orders/${this.props.props.params.order_id}`)
@@ -26,21 +26,31 @@ export default class OrderComponent extends React.Component {
     render() {
         const order = this.state.order;
         return (
-            <div className="order-container">
+            <div className="container">
+
                 {order && order.user ? <h1>Order Made By: {order.user.name}</h1> : ''}
-                <h1>Order Status: {order.status}</h1>
-                <h1>Order Address: {order.address}</h1>
+                <div>
+                    <h1>Order Status: {order.status}</h1>
+                    <h1>Shipping address: {order.address}</h1>
+                </div>
                 {
+
                     order.products && order.products.map((product, index) => {
                         const order_product = product.order_product;
                         this.state.total += Number(order_product.subTotal);
                         return (
-                            <div className={`order-product-${index}`} key={index}>
-                                <Link to={`/products/${product.id}`}>
-                                    <p>{product.title}</p>
-                                </Link>
-                                <p>Quantity: {order_product.quantity}</p>
-                                <p>Subtotal: ${order_product.subTotal}</p>
+                            <div className="row">
+                                <div className="col-md-4">
+                                    <div className="panel panel-default text-center">
+                                        <div className={`order-product-${index}`} key={index}>
+                                            <Link to={`/products/${product.id}`}>
+                                                <p>{product.title}</p>
+                                            </Link>
+                                            <p>Quantity: {order_product.quantity}</p>
+                                            <p>Subtotal: ${order_product.subTotal}</p>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         );
                     })
