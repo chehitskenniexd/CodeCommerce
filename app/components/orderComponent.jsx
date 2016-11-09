bae2b508a0a7ed89f4373c758fa24bae45adabf0
George Smith-Sweeper <george.smsweeper@gmail.com>
2016-11-08 22:21:36 -0500

    orderComponent has been styled /users/:id/orders/:id

--- app/components/orderComponent.jsx
+++ app/components/orderComponent.jsx
@@ -26,37 +26,65 @@ export default class OrderComponent extends React.Component {
     render() {
         const order = this.state.order;
         return (
-            <div className="container">
+            <div className="row">
+                <div className="panel panel-default">
+                    <div className="panel-heading">
+                        <h3 className="text-center"><strong>Order Summary</strong></h3>
+                    </div>
+                    <div className="panel-body">
+                        <div className="table-responsive">
+                            <table className="table table-condensed">
+                                <thead>
+                                    <tr>
+                                        <td><strong>Item Name</strong></td>
+                                        <td className="text-center"><strong>Item Price</strong></td>
+                                        <td className="text-center"><strong>Item Quantity</strong></td>
+                                        <td className="text-right"><strong>Total</strong></td>
+                                    </tr>
+                                </thead>
+                                <tbody>
 
-                {order && order.user ? <h1>Order Made By: {order.user.name}</h1> : ''}
-                <div>
-                    <h1>Order Status: {order.status}</h1>
-                    <h1>Shipping address: {order.address}</h1>
-                </div>
-                {
+                                    {
 
-                    order.products && order.products.map((product, index) => {
-                        const order_product = product.order_product;
-                        this.state.total += Number(order_product.subTotal);
-                        return (
-                            <div className="row">
-                                <div className="col-md-4">
-                                    <div className="panel panel-default text-center">
-                                        <div className={`order-product-${index}`} key={index}>
-                                            <Link to={`/products/${product.id}`}>
-                                                <p>{product.title}</p>
-                                            </Link>
-                                            <p>Quantity: {order_product.quantity}</p>
-                                            <p>Subtotal: ${order_product.subTotal}</p>
-                                        </div>
-                                    </div>
-                                </div>
+                                        order.products && order.products.map((product, index) => {
+                                            const order_product = product.order_product;
+                                            this.state.total += Number(order_product.subTotal);
+                                            return (
+                                                <tr key={index}>
+                                                    <td className="text-left">{product.title}</td>
+                                                    <td className="text-center">${product.price}</td>
+                                                    <td className="text-center">{order_product.quantity}</td>
+                                                    <td className="text-right">${order_product.subTotal}</td>
+                                                </tr>
+                                            );
+                                        })
+                                    }
+
+                                </tbody>
+                            </table>
+                            <h3 style={{"textAlign": "right"}}>Total: ${this.state.total}</h3>
+                            <div>
+                                {order && order.user ? <h4>Thank you: {order.user.name}</h4> : ''}
                             </div>
-                        );
-                    })
-                }
-                <h1>Grand Total: ${this.state.total}</h1>
+                            <div>
+                                <p>Order Status: {order.status}</p>
+                            </div>
+                            <div>
+                                <p>Shipping address: {order.address}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
 }
+
+
+/*
+   {order && order.user ? <h1>Order Made By: {order.user.name}</h1> : ''}
+                                <div>
+                                    <h1>Order Status: {order.status}</h1>
+                                    <h1>Shipping address: {order.address}</h1>
+                                </div>*/
+
