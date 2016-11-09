c1669057823c9343ec2f6d203c6191c8ce99ae5f
Kenneth Moy <moykenneth91@gmail.com>
2016-11-08 19:55:47 -0500

    fixed an error with categories not filtering

--- server/routes/categoryRoutes.js
+++ server/routes/categoryRoutes.js
@@ -2,17 +2,30 @@
 
 const epilogue = require('../epilogue')
 const db = require('APP/db')
+const Category = db.model('category');
 
 const customCategoryRoutes = require('express').Router()
 
 // Custom routes go here.
+customCategoryRoutes.get('/:id', (req, res, next) => {
+  Category.findById(req.params.id, {
+    include: [
+      {all: true}
+    ]
+  })
+    .then(category => {
+      res.status(201).json(category);
+    })
+    .catch(err => console.log('Error getting category!', err));
+})
 
 module.exports = customCategoryRoutes
 
 // Epilogue will automatically create standard RESTful routes
 const category = epilogue.resource({
   model: db.model('category'),
-  endpoints: ['/category', '/category/:id']
+  endpoints: ['/category', '/category/:id'],
+  actions: ['list', 'delete']
 })
 
 // find all users
@@ -21,11 +34,11 @@ category.list = (req, res, context) => {
 }
 
 // get details about one user
-category.read = (req, res, context) => {
-  const categoryId = req.params.id;
-  aCategory = context.find(category => category.id === categoryId);
-  res.status(201).json(aCategory);
-}
+// category.read = (req, res, context) => {
+//   const categoryId = req.params.id;
+//   aCategory = context.find(category => category.id === categoryId);
+//   res.status(201).json(aCategory);
+// }
 
 const {mustBeLoggedIn, selfOnly, forbidden} = epilogue.filters
 category.delete.auth(mustBeLoggedIn)
